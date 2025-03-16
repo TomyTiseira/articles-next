3c130bdf61be79a6abd9f2f3b2dcaddb3cfd6ed6
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-03-16 01:22:34 -0300

    Remove NotFound component and refactor page to utilize Next.js notFound function for improved routing

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,6 @@
-import { PageNotFound } from '@/components';
-// import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
-// interface Props {
-//   params: {
-//     id: string;
-//   }
-// }
 
 export default function Home() {
-  // const { id } = params;
-
-  // if ( id ) {
-  //   notFound();
-  // }
-
-
-  return (
-    <PageNotFound />
-  );
+  notFound();
 }
