0cd8abd17b20e5e0721501636d6febc87163c481
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-03-16 02:46:07 -0300

    Refactor QuantitySelector to use onQuantityChange prop and update count with useEffect

--- src/components/ui/quantity-selector/QuantitySelector.tsx
+++ src/components/ui/quantity-selector/QuantitySelector.tsx
@@ -1,16 +1,21 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoAddOutline, IoRemoveOutline } from 'react-icons/io5';
 import styles from './quantity-selector.module.css';
 
 interface Props {
   quantity: number;
+  onQuantityChange: (quantity: number) => void;
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
+export const QuantitySelector = ({ quantity, onQuantityChange }: Props) => {
   const [count, setCount] = useState(quantity);
 
-  const onQuantityChange = (value: number) => {
+  useEffect(() => {
+    onQuantityChange(count);
+  }, [count, onQuantityChange]);
+
+  const handleQuantityChange = (value: number) => {
     if (count + value < 1) return;
     if (count + value > 10) return;
 
@@ -21,7 +26,7 @@ export const QuantitySelector = ({ quantity }: Props) => {
     <div className={styles.container}>
       {/* Botón para disminuir la cantidad */}
       <button
-        onClick={() => onQuantityChange(-1)}
+        onClick={() => handleQuantityChange(-1)}
         className={styles.button}
       >
         <IoRemoveOutline className={styles.icon} />
@@ -32,7 +37,7 @@ export const QuantitySelector = ({ quantity }: Props) => {
 
       {/* Botón para aumentar la cantidad */}
       <button
-        onClick={() => onQuantityChange(1)}
+        onClick={() => handleQuantityChange(1)}
         className={styles.button}
       >
         <IoAddOutline className={styles.icon} />
