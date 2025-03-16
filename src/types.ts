1194325795895b528286f9b5b18d545709d0db5d
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-03-16 02:45:05 -0300

    Add CartItem interface to define structure for items in the cart

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,11 @@ export interface Article {
 }
 
 export type Category = 'Computadoras' | 'Accesorios' | 'Electrónica';
+
+export interface CartItem {
+  id: number;
+  titulo: string;
+  imagen: string;
+  precio: number;
+  cantidad: number;
+}
