1a92cb54bf49a8933c372fd3c65b2a58b4b0f2a9
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-03-16 02:46:02 -0300

    Replace sidebar links with CartPopup component for improved cart access

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,15 +1,8 @@
 'use client';
-import Link from 'next/link';
 import React from 'react';
-import {
-  IoCloseOutline,
-  IoHeadsetOutline,
-  IoHomeOutline,
-  IoLaptopOutline,
-  IoPhonePortraitOutline,
-} from 'react-icons/io5';
 import styles from './sidebar.module.css';
 import { useUiStore } from '@/store';
+import { CartPopup } from '@/components/cart/CartPopup';
 
 export default function Sidebar() {
   const isSideMenuOpen = useUiStore(state => state.isSideMenuOpen);
@@ -31,49 +24,7 @@ export default function Sidebar() {
       <nav
         className={`${styles.sidebar} ${isSideMenuOpen ? styles.sidebarOpen : ''}`}
       >
-        {/* Botón de cierre */}
-        <IoCloseOutline
-          size={30}
-          className={styles.closeButton}
-          onClick={closeSideMenu}
-        />
-
-        {/* Enlaces del menú */}
-        <Link
-          href="/"
-          className={styles.menuLink}
-          onClick={closeSideMenu}
-        >
-          <IoHomeOutline size={24} className={styles.icon} />
-          <span className={styles.linkText}>Inicio</span>
-        </Link>
-
-        <Link
-          href="/category/Electrónica"
-          className={styles.menuLink}
-          onClick={closeSideMenu}
-        >
-          <IoPhonePortraitOutline size={24} className={styles.icon} />
-          <span className={styles.linkText}>Electrónica</span>
-        </Link>
-
-        <Link
-          href="/category/Accesorios"
-          className={styles.menuLink}
-          onClick={closeSideMenu}
-        >
-          <IoHeadsetOutline size={24} className={styles.icon} />
-          <span className={styles.linkText}>Accesorios</span>
-        </Link>
-
-        <Link
-          href="/category/Computadoras"
-          className={styles.menuLink}
-          onClick={closeSideMenu}
-        >
-          <IoLaptopOutline size={24} className={styles.icon} />
-          <span className={styles.linkText}>Computadoras</span>
-        </Link>
+        <CartPopup onClose={closeSideMenu}/>
       </nav>
     </>
   );
